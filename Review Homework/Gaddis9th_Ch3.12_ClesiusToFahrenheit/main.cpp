43ef5f57057677b040a96d186b21a0ea8a2dd291
PhysicsJoe92 <hernandezj478@yahoo.com>
2021-06-24 00:08:29 -0700

    Ch3.12
    Gaddis 9th Chapter 3 review question 12

---
+++ Review Homework/Gaddis9th_Ch3.12_ClesiusToFahrenheit/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * File: CtoF.cpp
+ * Author: Joseph Hernandez
+ * Created on 23 Jun 2021, 9:35 pm
+ * Purpose: To convert Celsius to Fahrenheit
+*/
+
+// System Libraries
+#include <iostream>
+using namespace std;
+
+// Code bedins here
+int main() {
+    // Variables
+    float Cels, Fahr;
+
+    // Get user to enter degrees in Celsius
+    cout << "Please enter the degrees in Celsius: ";
+    cin >> Cels;
+    cout << endl;
+
+    // Convert to Fahrenheit
+    Fahr = (9.0 / 5.0) * Cels + 32;
+
+    // Display temperature
+    cout << "The temperature in Fahrenheit is " << Fahr << "." << endl;
+
+    return 0;
+}
